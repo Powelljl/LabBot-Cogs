a96e5a793418c883fe07e89bc0cfbe351707a155
Keir Davis <keirjdavis@gmail.com>
2020-04-13 21:37:07 +0100

    feat(jail): add base jail and bail commands

--- jail/jail.py
+++ jail/jail.py
@@ -1,5 +1,6 @@
 """discord red-bot jail"""
-from redbot.core import commands, Config
+from redbot.core import commands, Config, checks
+import discord
 
 
 class JailCog(commands.Cog):
@@ -8,3 +9,25 @@ class JailCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.settings = Config.get_conf(self, identifier=1249812384)
+
+    @commands.command(name="jail")
+    @commands.guild_only()
+    @checks.mod()
+    async def jail(
+        self,
+        ctx: commands.Context,
+        user: discord.Member,
+        *,
+        time: int = None
+    ):
+        pass
+
+    @commands.command(name="bail")
+    @commands.guild_only()
+    @checks.mod()
+    async def bail(
+        self,
+        ctx: commands.Context,
+        user: discord.Member
+    ):
+        pass
